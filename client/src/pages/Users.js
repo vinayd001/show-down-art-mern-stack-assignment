fbd7eb6ba94650b84e08c1147a2b9d821f191e9c
Vinay Dangwal <vinaydangwal001@gmail.com>
2022-02-11 17:51:25 +0530

    users data in table format

--- client/src/pages/Users.js
+++ client/src/pages/Users.js
@@ -3,6 +3,19 @@ import { useEffect, useState } from "react";
 const Users = () => {
   const [userList, setUserList] = useState([]);
 
+  const table = {
+    borderCollapse: "collapse",
+    width: "100%",
+  };
+
+  const th = {
+    border: "1px solid #dddddd",
+    textAlign: "left",
+    padding: "8px",
+  };
+
+  const td = th;
+
   useEffect(() => {
     fetch("/users")
       .then((res) => res.json())
@@ -21,6 +34,26 @@ const Users = () => {
       <h1>User List</h1>
       <br />
       {userList.length > 0 ? (
+        <table>
+          <tbody>
+            <tr>
+              <th style={th}>Email</th>
+              <th style={th}>Fullname</th>
+              <th style={th}>Username</th>
+            </tr>
+            {userList.map((user) => (
+              <tr key={user._id}>
+                <td style={td}>{user?.email}</td>
+                <td style={th}>{user?.fullname}</td>
+                <td style={th}>{user?.username}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div>No users found</div>
+      )}
+      {/* {userList.length > 0 ? (
         userList.map((user, index) => (
           <div key={user._id}>
             <div>Full Name: {user?.fullname}</div>{" "}
@@ -30,7 +63,7 @@ const Users = () => {
         ))
       ) : (
         <div>No users found</div>
-      )}
+      )} */}
     </div>
   );
 };
